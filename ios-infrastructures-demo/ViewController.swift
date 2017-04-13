3ba70db7c5666ca62e09ad839362de3f8be05b5e
y.okudera <mail@sample.com>
2017-04-14 02:39:55 +0900

    Initial Commit

---
+++ ios-infrastructures-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-infrastructures-demo
+//
+//  Created by OkuderaYuki on 2017/04/14.
+//  Copyright © 2017年 YukiOkudera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
